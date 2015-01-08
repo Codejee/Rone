80578bd59c88a7df67f5696c195e9b6a554711aa
Codejee <mail.pradeepkothari@gmail.com>
2015-01-08 18:12:18 +0530

    added comments to mean

--- mean.R
+++ mean.R
@@ -1,7 +1,9 @@
+#generates mean of first hundred random numbers
 myfunction <- function(){
         x <- rnorm(100)
          mean(x)
 }
+#adds noise in above result
 second <-function(x) {
        x + rnorm(length(x))
 }
